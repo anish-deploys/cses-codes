01e35bedc22763e1a2dfb8737f5cc3b9600872f3
Anish Bakshi <117637218+anish-deploys@users.noreply.github.com>
2025-04-12 00:03:08 +0530

    Add files via upload

---
+++ Weird_Algorithm.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#define ll long long
+using namespace std;
+int main() {
+    ll n;
+    cin>>n;
+
+    while(n!=1) {
+        cout<<n<<" ";
+        if (n % 2 == 0) n /= 2;
+        else n = n * 3 + 1;
+    }
+    cout<<1<<endl;
+    return 0;
+}
